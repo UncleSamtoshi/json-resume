bb7de6145799c0a32a037ce879ca7da8935967fa
Sam <sam@Sams-MacBook-Pro.local>
2021-12-17 21:37:37 -0600

    Adding docker file for development

--- index.js
+++ index.js
@@ -37,18 +37,18 @@ const resumes = resumeDefinitions.map(def => ({...def,resumeJson:filterResume(ba
 
 // Create json files, PDF's, and HTML
 resumes.forEach(resume => {
-    fs.writeFileSync("/out/",JSON.stringify(resume.resumeJson))
+    fs.writeFileSync(`${resume.name}.json`,JSON.stringify(resume.resumeJson))
 })
 
-// Create PDF's and html from resumes
-exec("resume export -r ./resume-updated.json ./bitcoinresume.pdf -t ./node_modules/jsonresume-theme-stackoverflow", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+// // Create PDF's and html from resumes
+// exec("resume export -r ./resume-updated.json ./bitcoinresume.pdf -t ./node_modules/jsonresume-theme-stackoverflow", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
